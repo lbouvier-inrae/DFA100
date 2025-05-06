7080980fc6cd96842093aa3afb6321342ede74bb
Maxime Gosselin <maxime.gosselin.etu@univ-lille.fr>
2025-05-06 11:15:07 +0200

    Le main qui permet de lancer l'application

---
+++ main.py
@@ -0,0 +1,9 @@
+from PySide6.QtWidgets import QApplication
+from interface.ui import VideoAnalyzerUI
+import sys
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = VideoAnalyzerUI()
+    window.show()
+    sys.exit(app.exec())
